73081937606e680e3aae11a69a821eabedd93d52
Terry <hl928452957@gmail.com>
2024-09-12 16:47:07 +0800

    test gpio to control led

--- GPIO.py
+++ GPIO.py
@@ -1,20 +1,19 @@
 # run on Pi
+# RPi.GPIO is a Python library that provides a Python interface to the Raspberry Pi GPIO.
+# if you want to run this code on macOS, you need to install the RPi.GPIO library.
+"""
+This Python code controls an LED connected to GPIO pin 17 on a Raspberry Pi.
+ It uses the Broadcom (BCM) numbering system, sets pin 17 as an output,
+ turns the LED on for 5 seconds, and then turns it off.
+ Afterward, it resets the GPIO settings to their default state
+ to prevent any issues with lingering signals.
+"""
 import RPi.GPIO as GPIO
 import time
-
-
 GPIO.setmode(GPIO.BCM)
-
-
 led_pin = 17
-
-
 GPIO.setup(led_pin, GPIO.OUT)
-
 GPIO.output(led_pin, GPIO.HIGH)
 time.sleep(5)
-
-
 GPIO.output(led_pin, GPIO.LOW)
-
 GPIO.cleanup()
--- remoteGPIO.py
+++ remoteGPIO.py
@@ -1,7 +1,10 @@
 
 # 	1.	Connect the long leg of the LED to the GPIO pin of the Raspberry Pi (e.g., GPIO 17 corresponds to pin 11).
 # 	2.	Connect the short leg of the LED to the ground (GND, Raspberry Pi pin 6) through a 220 ohm resistor.
-
+"""
+This Python script uses the gpiozero library to control
+an LED connected to a Raspberry Pi GPIO pin via a remote host
+"""
 from gpiozero import LED
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
